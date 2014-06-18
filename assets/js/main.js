5b6f94d0f4df34e0df1f83c9dc74c2e7e4a9006e
Shivek Khurana <khuranashivek@gmail.com>
2014-06-18 08:34:15 +0530

    There can only be one container

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,14 @@
+jQuery.fn.verticalAlign = function ()
+{
+    return this
+            .css("margin-top",($(this).parent()
+            .height() - $(this).height())/2 + 'px' )
+};
+
 $(function(){
 	var BV = new $.BigVideo({doLoop:true});
 	BV.init();
 	BV.show("assets/bg_video.mp4");
+
+	$('.description').verticalAlign();
 });
