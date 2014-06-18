ad8fa01fd399a4c48c6e0aa17e97a5828437c2ea
Shivek Khurana <khuranashivek@gmail.com>
2014-06-18 09:27:35 +0530

    All ok except the video stretches

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,8 +10,15 @@ $(function(){
 	BV.init();
 	BV.show("assets/bg_video.mp4");
 
+	$('#big-video-wrap').css({
+		width:$(window).width(),
+		height:$(window).height()
+	});
+
+
 	$('.intro_text_container').css({height:$(window).height()-200 + "px"});
 	$('#intro_text').verticalAlign();
 
+	//$('.screens').css({"margin-top":$('.intro_text_container').height()});
 	//$('.description').verticalAlign();
 });
