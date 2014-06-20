7f117dc49924ccd5444c763b0248bc340bebb0f5
Shivek Khurana <khuranashivek@gmail.com>
2014-06-20 12:13:33 +0530

    Added bqjs slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,31 @@
 jQuery.fn.verticalAlign = function ()
 {
     return this
-            .css("margin-top",($(this).parent()
+            .css('margin-top',($(this).parent()
             .height() - $(this).height())/2 + 'px' )
 };
 
 $(function(){
 	var BV = new $.BigVideo({doLoop:true});
 	BV.init();
-	BV.show("assets/bg_video.mp4");
+	BV.show('assets/bg_video.mp4');
 
-	$("#loader").verticalAlign();
+	$('#loader').verticalAlign();
 
 	BV.getPlayer().on('durationchange',function(){
-		$("#loader").fadeOut(0);
+		$('#loader').fadeOut(0);
 		$('#big-video-wrap').fadeIn();
 		$('#container').fadeIn(0);
-		new WOW().init();
 	});
 
 
-	$('.intro_text_container').css({height:$(window).height()-200 + "px"});
+	$('.intro_text_container').css({height:$(window).height()-200 + 'px'});
 	$('#intro_text').verticalAlign();
 
+	$('#screens').bjqs({
+		'height' : $(window).height() + 'px',
+		'width' : $(window).width() + 'px',
+		'responsive' : true,
+		animspeed : 2000
+	});
 });
